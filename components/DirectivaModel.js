949bad9ba66b01d977f2e62e988d72e0d3055de2
Carlos <carlos.joaquin.gutierrez@gmail.com>
2019-08-20 11:12:39 +0200

    [mod]  directivas ejemplo final

--- components/DirectivaModel.js
+++ components/DirectivaModel.js
@@ -12,10 +12,15 @@ let DirectivaModel = {
             </label>
 
             <h2>Películas</h2>
-            <label v-for="(movie,key) in movies" :key=""key>
+            <label v-for="(movie,key) in movies" :key="key">
                 <input :value="movie" type="checkbox" v-model="favoritesMovies">         
                 {{ movie }}
             </label>
+
+            <h2 v-show = " favoritesMovies.length > 0 ">Películas favoritas</h2>
+            <ul>
+                <li v-for="(movie,key) in favoritesMovies" :key="key"> {{movie}} </li>
+            </ul>            
         </div>
         `,
     data() {
